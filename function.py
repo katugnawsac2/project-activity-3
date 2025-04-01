d9edf76e67515a1a510800e65b50de053d061003
Aeyobro <158158350+katugnawsac2@users.noreply.github.com>
2025-04-01 20:53:54 +0800

    Functiong using python

---
+++ function.py
@@ -0,0 +1,9 @@
+# Define the function
+def function_name():
+    print("This is an independent function.")
+
+# Call the function
+function_name()
+
+# Team Name: TEAM NINJA
+# Members: Joshua Therence Templo, Niña Jane Campaner, Marverick Catigbe, Hannah Jamaica Vega
