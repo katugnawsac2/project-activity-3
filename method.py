7c3982e75cde0c107d67fc6a508664de94dde0c9
Catigbe24 <catigbe.marverick24@gmail.com>
2025-04-02 20:46:15 +0800

    Method in python

---
+++ method.py
@@ -0,0 +1,24 @@
+# Define the class
+class MyClass:
+    # Define a method
+    def method1(self):
+        print("Method 1 is called.")
+
+    # Define another method
+    def method2(self):
+        print("Method 2 is called.")
+
+    # Define yet another method
+    def method3(self):
+        print("Method 3 is called.")
+
+# Instantiate the class
+my_object = MyClass()
+
+# Call the methods using the object
+my_object.method1()
+my_object.method2()
+my_object.method3()
+
+# Team Name: TEAM NINJA
+# Members: Joshua Therence Templo, Niña Jane Campaner, Marverick Catigbe, Hannah Jamaica Vega
