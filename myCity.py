ded79e92d7a1728d9c69bdcbad649bcd59eb7527
Catigbe24 <catigbe.marverick24@gmail.com>
2025-04-04 10:58:16 +0800

    Checking City using Python

---
+++ myCity.py
@@ -0,0 +1,11 @@
+# Define the function
+def myCity(city):
+    print("I live in " + city + ".")
+
+# Call the function with different city names
+myCity("Austin")
+myCity("Tokyo")
+myCity("Salzburg")
+
+# Team Name: TEAM NINJA
+# Members: Joshua Therence Templo, Niña Jane Campaner, Marverick Catigbe, Hannah Jamaica Vega
